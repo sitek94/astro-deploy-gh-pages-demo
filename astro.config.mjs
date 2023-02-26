283f01eca1c795b4e37b9940909a5ca6e516fe07
Maciek Sitkowski <msitkowski94@gmail.com>
2023-02-26 07:52:01 +0100

    init

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import {defineConfig} from 'astro/config'
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://sitek94.github.io',
+  base: '/astro-deploy-gh-pages-demo',
+})
